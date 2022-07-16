dd6286c9fcc2d62442e244947827a557ed06310d
Michael Federczuk <federczuk.michael@protonmail.com>
2022-07-16 23:03:48 +0200

    Add magnitude and direction stuff to `Vector2D`

--- src/geometry2d.ts
+++ src/geometry2d.ts
@@ -1,4 +1,19 @@
 /**
+ * Returns the given number in the range of `[0, 360)`.
+ */
+function keepDegreeInRange(n: number): number {
+	return keepInRange(n, 360);
+}
+
+function degreesToRadians(degrees: number): number {
+	return (degrees * Math.PI / 180);
+}
+
+function radiansToDegrees(radians: number): number {
+	return (radians * 180 / Math.PI);
+}
+
+/**
  * A 2-dimensional position/point.
  */
 class Position2D {
@@ -78,12 +93,15 @@ class Box2D {
  * A 2-dimensional euclidean vector.
  *
  * Ordinarily, vectors are represented by a magnitude and a direction, this implementation, though, uses two magnitudes;
- * one for the X-axis and one for the Y-axis -- it is basically two 1-dimensional vectors.
+ * one for the X-axis and one for the Y-axis -- it is basically two 1-dimensional vectors at a right angle to one
+ * another.
  *
  * @see https://en.wikipedia.org/wiki/Euclidean_vector
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class Vector2D {
+	// TODO: change to `representation: ({ xd: number, yd: number } | { magnitude: number, direction: number })`?
+
 	xd: number;
 	yd: number;
 
@@ -110,6 +128,40 @@ class Vector2D {
 		return new Vector2D(0, 0);
 	}
 
+	/**
+	 * @param direction Direction in degrees.
+	 *                  May be less than 0 or greater than- or equal to 360 -- will be put back into
+	 *                  the range of `[0, 360)`.
+	 */
+	static ofMagnitudeAndDirection(magnitude: number, direction: number): Vector2D {
+		// i'm not particular good at math, so there's *definitely* a *way* better way to do this
+
+		direction = degreesToRadians(keepDegreeInRange(direction));
+
+		if(direction < (Math.PI / 2)) {
+			const xd = magnitude * Math.sin(direction);
+			const yd = magnitude * Math.sin((Math.PI / 2) - direction);
+			return new Vector2D(xd, yd);
+		}
+
+		if(direction < Math.PI) {
+			const xd = magnitude * Math.sin(Math.PI - direction);
+			const yd = magnitude * Math.sin(direction - (Math.PI / 2));
+			return new Vector2D(xd, -yd);
+		}
+
+		if(direction < (Math.PI * 1.5)) {
+			const xd = magnitude * Math.sin(direction - Math.PI);
+			const yd = magnitude * Math.sin((Math.PI * 1.5) - direction);
+			return new Vector2D(-xd, -yd);
+		}
+
+		// direction < Math.PI * 2
+		const xd = magnitude * Math.sin((Math.PI * 2) - direction);
+		const yd = magnitude * Math.sin(direction - (Math.PI * 1.5));
+		return new Vector2D(-xd, yd);
+	}
+
 	setXdYd(xd: number, yd: number) {
 		this.xd = xd;
 		this.yd = yd;
@@ -127,6 +179,52 @@ class Vector2D {
 		}
 
 		return sum;
+	}
+
+	/**
+	 * Returns the magnitude of the vector.
+	 */
+	computeMagnitude(): number {
+		return Math.sqrt(Math.abs(this.xd) ** 2 + Math.abs(this.yd) ** 2);
+	}
+
+	/**
+	 * Returns the direction of the vector in degrees. `[0, 360)`
+	 */
+	computeDirection(): number {
+		return radiansToDegrees(this.#computeDirectionRad());
+	}
+
+	#computeDirectionRad(): number {
+		// i'm not particular good at math, so there's *definitely* a *way* better way to do this
+
+		if((this.xd >= 0) && (this.yd > 0)) {
+			return Math.atan(this.xd / this.yd);
+		}
+
+		if((this.xd > 0) && (this.yd <= 0)) {
+			return Math.atan(Math.abs(this.yd) / this.xd) + (Math.PI / 2);
+		}
+
+		if((this.xd <= 0) && (this.yd < 0)) {
+			return Math.atan(Math.abs(this.xd) / Math.abs(this.yd)) + Math.PI;
+		}
+
+		if((this.xd < 0) && (this.yd >= 0)) {
+			return Math.atan(this.yd / Math.abs(this.xd)) + (Math.PI * 1.5);
+		}
+
+		return 0; // zero vector
+	}
+
+	changeMagnitude(newMagnitude: number) {
+		// TODO: lazy way to do this, change it
+		this.assign(Vector2D.ofMagnitudeAndDirection(newMagnitude, this.computeDirection()));
+	}
+
+	changeDirection(newDirection: number) {
+		// TODO: lazy way to do this, change it
+		this.assign(Vector2D.ofMagnitudeAndDirection(this.computeMagnitude(), newDirection));
 	}
 
 	add(other: Readonly<Vector2D>) {
--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,12 @@ function error(msg: string): never {
 	window.alert(msg);
 	throw Error(msg);
 }
+
+/**
+ * Returns the given number in the range of `[0, exclusiveCeil)`.
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function keepInRange(n: number, exclusiveCeil: number): number {
+	// looks stupid, but it works
+	return ((n % exclusiveCeil) + exclusiveCeil) % exclusiveCeil;
+}
