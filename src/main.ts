e2ec5dcd0c09730963f7037b4997d5e3a39e6118
Michael Federczuk <federczuk.michael@protonmail.com>
2022-07-16 19:59:09 +0200

    Replace controls alert with text + icons

--- src/main.ts
+++ src/main.ts
@@ -221,6 +221,4 @@ window.onload = () => {
 		setInterval(fpsWatcherCallback, 1000);
 		frameCallback();
 	});
-
-	window.alert("Move the subject left/right with [A]/[D] and jump with [Spacebar]");
 };
