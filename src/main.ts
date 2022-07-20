37f143da881b08e25d1a1164dcc10455ca90ec5a
Michael Federczuk <federczuk.michael@protonmail.com>
2022-07-20 03:13:58 +0200

    Improve controls guide

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+const CONTROLS_GUIDE_ACTIVE_CLASS_NAME = "controls-guide-active";
+
+
 // global state so that it can be manipulated using the browser console
 const state = new State();
 
@@ -9,6 +12,17 @@ state.addNewEntity("Wug",  new Box2D(0, 0, 30, 125), 5, undefined, undefined, un
 
 
 window.onload = () => {
+	const normalControlsGuide: (HTMLElement | null) = document.getElementById("normal-controls-guide");
+	const noclipControlsGuide: (HTMLElement | null) = document.getElementById("noclip-controls-guide");
+
+	if(normalControlsGuide === null) {
+		error("Normal controls guide table (#normal-controls-guide) not found");
+	}
+	if(noclipControlsGuide === null) {
+		error("Noclip controls guide table (#noclip-controls-guide) not found");
+	}
+
+
 	const [canvas, context]: [HTMLCanvasElement, CanvasRenderingContext2D] = initCanvas();
 
 	const adjustBounds = () => {
@@ -39,6 +53,27 @@ window.onload = () => {
 		lastFps = frameCount;
 		frameCount = 0;
 	};
+
+
+	normalControlsGuide.onanimationend = () => normalControlsGuide.classList.remove(CONTROLS_GUIDE_ACTIVE_CLASS_NAME);
+	noclipControlsGuide.onanimationend = () => noclipControlsGuide.classList.remove(CONTROLS_GUIDE_ACTIVE_CLASS_NAME);
+
+	state.subject.addNoclipChangeListener({ invokeImmediately: true }, (noclip: boolean) => {
+		if(!(state.subject.controller instanceof WebKeyboardController)) {
+			normalControlsGuide.classList.remove(CONTROLS_GUIDE_ACTIVE_CLASS_NAME);
+			noclipControlsGuide.classList.remove(CONTROLS_GUIDE_ACTIVE_CLASS_NAME);
+			return;
+		}
+
+		if(!noclip) {
+			normalControlsGuide.classList.add(CONTROLS_GUIDE_ACTIVE_CLASS_NAME);
+			noclipControlsGuide.classList.remove(CONTROLS_GUIDE_ACTIVE_CLASS_NAME);
+		} else {
+			normalControlsGuide.classList.remove(CONTROLS_GUIDE_ACTIVE_CLASS_NAME);
+			noclipControlsGuide.classList.add(CONTROLS_GUIDE_ACTIVE_CLASS_NAME);
+		}
+	});
+
 
 	window.requestAnimationFrame(() => {
 		setInterval(fpsWatcherCallback, 1000);
