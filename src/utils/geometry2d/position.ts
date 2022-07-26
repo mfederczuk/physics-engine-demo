fcfbba50d89c520258c213175c13c84de7db6465
Michael Federczuk <federczuk.michael@protonmail.com>
2022-07-26 08:51:42 +0200

    Add `ReadonlyPosition2D` interface

--- src/utils/geometry2d/position.ts
+++ src/utils/geometry2d/position.ts
@@ -3,11 +3,17 @@
  * SPDX-License-Identifier: MPL-2.0 AND Apache-2.0
  */
 
+interface ReadonlyPosition2D {
+	readonly x: number;
+	readonly y: number;
+}
+
 /**
  * A 2-dimensional position/point.
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-class Position2D {
+class Position2D implements ReadonlyPosition2D {
+
 	x: number;
 	y: number;
 
@@ -16,7 +22,7 @@ class Position2D {
 		this.y = y;
 	}
 
-	assign(other: Readonly<Position2D>) {
+	assign(other: ReadonlyPosition2D) {
 		this.x = other.x;
 		this.y = other.y;
 	}
